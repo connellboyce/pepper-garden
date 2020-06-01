5b29cb2ef86284349f6e112608a8769f169c0b1a
Connell Boyce <connell.p.boyce@gmail.com>
2020-06-01 13:04:32 -0400

    Intializing Spring Components

---
+++ src/main/java/com/connellboyce/peppergarden/PepperGardenApplication.java
@@ -0,0 +1,13 @@
+package com.connellboyce.peppergarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepperGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepperGardenApplication.class, args);
+	}
+
+}
