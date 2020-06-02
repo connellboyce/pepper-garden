401cc253a9b0d67245c08f6db0f96ce7a2d08632
Connell Boyce <connell.p.boyce@gmail.com>
2020-06-02 16:45:11 -0400

    Adding Spring Security functionality

---
+++ src/main/java/com/connellboyce/peppergarden/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.connellboyce.peppergarden.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.connellboyce.peppergarden.model.ERole;
+import com.connellboyce.peppergarden.model.Role;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(ERole name);
+}
