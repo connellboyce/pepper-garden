39a70f6ab9f32345f44aee630df3af84499b3bb7
Connell Boyce <connell.p.boyce@gmail.com>
2020-06-09 10:03:12 -0400

    Added GET functionality to pepperAPI
    - To do: throw 404 if no pepper exists

--- src/main/java/com/connellboyce/peppergarden/repository/PepperRepository.java
+++ src/main/java/com/connellboyce/peppergarden/repository/PepperRepository.java
@@ -1,10 +1,16 @@
 package com.connellboyce.peppergarden.repository;
 
 import com.connellboyce.peppergarden.model.Pepper;
+import org.springframework.data.domain.Example;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface PepperRepository extends MongoRepository<Pepper, String> {
     Optional<Pepper> findByName(String name);
     Boolean existsByName(String name);
+
+    @Override
+    <S extends Pepper> Optional<S> findOne(Example<S> example);
 }
