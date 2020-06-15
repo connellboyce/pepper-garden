b1bc2c3aeec45b5e0deae248f0dfcb581778d13d
Connell Boyce <connell.p.boyce@gmail.com>
2020-06-15 16:38:02 -0400

    Added GET functionality and display features

--- src/main/java/com/connellboyce/peppergarden/controller/BlogController.java
+++ src/main/java/com/connellboyce/peppergarden/controller/BlogController.java
@@ -1,6 +1,7 @@
 package com.connellboyce.peppergarden.controller;
 
 import com.connellboyce.peppergarden.model.BlogPost;
+import com.connellboyce.peppergarden.model.Pepper;
 import com.connellboyce.peppergarden.payload.request.BlogRequest;
 import com.connellboyce.peppergarden.payload.response.MessageResponse;
 import com.connellboyce.peppergarden.repository.BlogRepository;
@@ -8,12 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/blog")
@@ -23,18 +24,31 @@ public class BlogController {
     @Autowired
     private BlogRepository blogRepository;
 
-    @PostMapping("/")
+    @PostMapping("/add")
     public ResponseEntity<?> makePost(@Valid @RequestBody BlogRequest blogRequest) {
-        if (blogRepository.existsBySlug(blogRequest.getSlug())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Error: URL already exists!"));
-        }
-
         BlogPost blogPost = new BlogPost(blogRequest.getSlug(), blogRequest.getTitle(), blogRequest.getBody(), blogRequest.getTags());
         logger.info(blogPost.toString());
         blogRepository.save(blogPost);
 
         return ResponseEntity.ok(new MessageResponse("Post blogged successfully!"));
+    }
+
+    @GetMapping("/{slug}")
+    public BlogPost getPepperById(@PathVariable("slug")String slug) {
+        Optional<BlogPost> blogPost = blogRepository.findBySlug(slug);
+
+        //To do: throw 404 not found if else
+        return blogPost.orElse(null);
+    }
+
+    @GetMapping("/")
+    public List<BlogPost> getAllPosts() {
+        List<BlogPost> blogPostList = blogRepository.findAll();
+        blogPostList.forEach(e -> {
+            logger.debug(e.toString());
+        });
+
+
+        return blogPostList;
     }
 }
