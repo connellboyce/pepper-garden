a56e2600e85e8d5ca8dd8b0dbb9f1df6d7f02f72
Connell Boyce <connell.p.boyce@gmail.com>
2020-06-19 10:51:38 -0400

    Fixed a bug where description was not being added
    - Model class had this.description = this.description instead of
      this.description = description

--- src/main/java/com/connellboyce/peppergarden/services/UserProfileService.java
+++ src/main/java/com/connellboyce/peppergarden/services/UserProfileService.java
@@ -1,10 +1,12 @@
 package com.connellboyce.peppergarden.services;
 
 import com.connellboyce.peppergarden.model.UserProfile;
+import com.connellboyce.peppergarden.payload.response.MessageResponse;
 import com.connellboyce.peppergarden.repository.UserProfileRepository;
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,20 +18,27 @@ public class UserProfileService {
     @Autowired
     private UserProfileRepository userProfileRepository;
 
-    public String editProfile(String userID, MultipartFile file, String zipCode, String hardinessZone, String description) throws IOException {
+    public ResponseEntity<?> editProfile(String id, MultipartFile file, String zipCode, String hardinessZone, String description) throws IOException {
         UserProfile userProfile = new UserProfile();
-        userProfile.setUserID(userID);
+        userProfile.setId(id);
         userProfile.setImage(new Binary(BsonBinarySubType.BINARY, file.getBytes()));
         userProfile.setZipCode(zipCode);
         userProfile.setHardinessZone(hardinessZone);
         userProfile.setDescription(description);
 
-        userProfile = userProfileRepository.insert(userProfile);
-        return userProfile.getId();
+        if (userProfileRepository.existsById(userProfile.getId())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Profile ID already exists!"));
+        }
+
+        userProfileRepository.insert(userProfile);
+        return ResponseEntity.ok(new MessageResponse("Success"));
 
     }
 
     public UserProfile getProfile(String id) {
-        return userProfileRepository.findById(id).get();
+        if (userProfileRepository.findById(id).isPresent()) {
+            return userProfileRepository.findById(id).get();
+        }
+        return null;
     }
 }
